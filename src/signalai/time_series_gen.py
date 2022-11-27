c89344ff67f41c202aa3f3e04c9d8e98f910fc3c
Martin Kovanda <kovanda.physics@gmail.com>
2022-11-26 19:55:13 -0700

    tsg

---
+++ src/signalai/time_series_gen.py
@@ -0,0 +1,177 @@
+import abc
+from functools import lru_cache
+from itertools import chain
+
+from time_series import TimeSeries, sum_time_series
+
+
+class TimeSeriesGen(abc.ABC):
+    def __init__(self, **config):
+        self.input_ts_gen_args: list[TimeSeriesGen] = []
+        self.input_ts_gen_kwargs: dict[any, TimeSeriesGen] = {}
+        self.taken_length = None
+        self.config = config
+        self.data = None
+        self.built = False
+        self.getitem = lru_cache(1)(self._getitem)
+
+    def _set_epoch(self, epoch_id: int):
+        pass
+
+    def set_epoch(self, epoch_id: int):
+        self._set_epoch(epoch_id)
+        for i in chain(self.input_ts_gen_args, self.input_ts_gen_kwargs.values()):
+            i.set_epoch(epoch_id)
+
+    def _build(self):  # expensive loading/building function for data
+        pass
+
+    def build(self):
+        if not self.built:
+            self._build()
+            self.built = True
+        for i in chain(self.input_ts_gen_args, self.input_ts_gen_kwargs.values()):
+            i.build()
+
+    def set_taken_length(self, length):
+        if self.taken_length is None:
+            self.taken_length = length
+        else:
+            if self.taken_length != length:
+                raise ValueError(
+                    f'This generator already gives length of {self.taken_length}, length {length} cannot '
+                    f'be set instead. To bypass, restart taken_length value by calling reset_taken_length().')
+
+        self._set_child_taken_length(length)
+
+    def _set_child_taken_length(self, length):
+        pass
+
+    def reset_taken_length(self):
+        self.taken_length = None
+        for i in chain(self.input_ts_gen_args, self.input_ts_gen_kwargs.values()):
+            i.reset_taken_length()
+
+    def __len__(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def _getitem(self, item: int) -> TimeSeries:
+        pass
+
+    @abc.abstractmethod
+    @lru_cache(1)
+    def is_infinite(self) -> bool:
+        pass
+
+
+class Transformer(TimeSeriesGen):
+    takes = None  # 'time_series', 'list', 'dict'
+
+    @abc.abstractmethod
+    def process(self, input_: TimeSeries | list[TimeSeries] | dict[str, TimeSeries]) -> TimeSeries:
+        pass
+
+    @abc.abstractmethod
+    @lru_cache(1)
+    def transform_taken_length(self, length: int) -> int:
+        pass
+
+    def _set_child_taken_length(self, length):
+        needed_length = self.transform_taken_length(length)
+        for i in chain(self.input_ts_gen_args, self.input_ts_gen_kwargs.values()):
+            i.set_taken_length(needed_length)
+
+    def __len__(self):
+        lens = {len(tsg) for tsg in chain(self.input_ts_gen_args, self.input_ts_gen_kwargs.values()) if not tsg.is_infinite()}
+        if len(lens) > 1:
+            raise ValueError('Generators appear to be of different lengths!')
+        return lens.pop()
+
+    def _getitem(self, item: int) -> TimeSeries:
+        if self.takes == 'time_series':
+            return self.process(self.input_ts_gen_args[0].getitem(item))
+        elif self.takes == 'list':
+            return self.process([tsg.getitem(item) for tsg in self.input_ts_gen_args])
+        elif self.takes == 'dict':
+            return self.process({key: tsg.getitem(item) for key, tsg in self.input_ts_gen_kwargs.items()})
+        else:
+            raise ValueError(f'Wrongly defined class, takes must be time_series/list/dict, not {self.takes}')
+
+    @lru_cache(1)
+    def is_infinite(self) -> bool:
+        return all([tsg.is_infinite() for tsg in chain(self.input_ts_gen_args, self.input_ts_gen_kwargs.values())])
+
+    def take_input(self, input_: TimeSeriesGen | list[TimeSeriesGen] | dict[str, TimeSeriesGen]) -> TimeSeriesGen:
+        transformer = type(self)(**self.config)
+        if self.takes == 'time_series':
+            assert isinstance(input_, TimeSeriesGen), f'Wrong input type {type(input_)}. Needed {self.takes}.'
+            transformer.input_ts_gen_args.append(input_)
+        elif self.takes == 'list':
+            assert isinstance(input_, list), f'Wrong input type {type(input_)}. Needed {self.takes}.'
+            assert all([isinstance(i, TimeSeriesGen) for i in input_])
+            transformer.input_ts_gen_args = input_
+        elif self.takes == 'dict':
+            assert isinstance(input_, dict), f'Wrong input type {type(input_)}. Needed {self.takes}.'
+            assert all([isinstance(val, TimeSeriesGen) and isinstance(key, str) for key, val in input_.items()])
+            transformer.input_ts_gen_kwargs = input_
+        else:
+            raise ValueError(f'Wrongly defined class, takes must be time_series/list/dict, not {self.takes}')
+        return transformer
+
+
+class TimeSeriesSum(Transformer):
+    takes = 'list'
+
+    def transform_taken_length(self, length: int) -> int:
+        pass
+
+    def process(self, ts_list: list[TimeSeries]) -> TimeSeries:
+        return sum_time_series(*ts_list)
+
+
+class LambdaTransformer(Transformer):
+    takes = 'time_series'
+
+    def _build(self):
+        assert 'lambda_x' in self.config
+        self.__getitem__ = lru_cache(50)(self._getitem)
+
+    def transform_taken_length(self, length: int) -> int:
+        return length
+
+    def process(self, ts: TimeSeries) -> TimeSeries:
+        func = eval(self.config['lambda_x'])
+        return ts.apply(func)
+
+
+def _graph_node(
+        time_series_gens: dict[str, TimeSeriesGen | Transformer],
+        structure: dict | list | str,
+) -> TimeSeriesGen | list[TimeSeriesGen] | dict[str, TimeSeriesGen]:
+
+    if isinstance(structure, str):
+        return time_series_gens[structure]
+    elif isinstance(structure, list):
+        return [_graph_node(time_series_gens, i) for i in structure]
+    elif isinstance(structure, dict):
+        if len(structure) == 1 and (base_tsg_name := list(structure.keys())[0])[0] != '=':
+            base_tsg: Transformer = time_series_gens[base_tsg_name]
+            return base_tsg.take_input(_graph_node(time_series_gens, structure[base_tsg_name]))
+        else:
+            resulting_dict = {}
+            for key, val in structure.items():
+                assert key[0] == '=', 'Names in dict must start with =.'
+                resulting_dict[key[1:]] = _graph_node(time_series_gens, val)
+            return resulting_dict
+    else:
+        raise TypeError(f"Type '{type(structure)}' of structure is not supported. Repair your config.")
+
+
+def make_graph(time_series_gens: dict[str, TimeSeriesGen], structure: dict) -> dict[str, TimeSeriesGen]:
+    output_gens = {}
+    for key, val in structure.items():
+        assert key[0] == '=', 'Names in dict must start with =.'
+        output_gens[key[1:]] = _graph_node({**time_series_gens, **output_gens}, val)
+
+    return output_gens
---
+++ tests/test_time_series_gen.py
@@ -0,0 +1,115 @@
+import numpy as np
+import yaml
+
+from time_series import TimeSeries, Signal
+from time_series_gen import TimeSeriesSum, TimeSeriesGen, LambdaTransformer, make_graph
+
+
+class SinusGenerator(TimeSeriesGen):
+    def _set_epoch(self, epoch_id: int):
+        self.config['epoch'] = epoch_id
+
+    def _build(self):
+        assert 'linspace_params' in self.config
+        assert 'amplitude' in self.config
+        assert 'built' not in self.config
+        self.config['built'] = True
+        self.history = []
+
+    def __len__(self):
+        assert ValueError
+
+    def _getitem(self, item: int) -> TimeSeries:
+        if len(self.history) > 0:
+            assert self.history[-1] != item
+        self.history.append(item)
+        return Signal(
+            data_arr=self.config['amplitude'] * np.sin(item+np.linspace(*self.config['linspace_params'])),
+            fs=self.config.get('fs'),
+        )
+
+    def is_infinite(self) -> bool:
+        return True
+
+    def __call__(self, input_):
+        raise ValueError('SinusGenerator does not take inputs.')
+
+
+class FiniteSinusGenerator(SinusGenerator):
+    def __len__(self):
+        return self.config['len_']
+
+    def _build(self):
+        assert 'len_' in self.config
+        super()._build()
+
+    def is_infinite(self) -> bool:
+        return False
+
+    def _getitem(self, item: int) -> TimeSeries:
+        if len(self.history) > 0:
+            assert self.history[-1] != item
+        self.history.append(item)
+        return Signal(
+            data_arr=self.config['amplitude'] * np.sin(item+np.linspace(*self.config['linspace_params'])),
+            fs=self.config.get('fs'),
+        )
+
+
+class NotUsed(FiniteSinusGenerator):
+    def _build(self):
+        assert False, 'This should not be run'
+
+
+def test_graph():
+    tsgs = {
+        'sg0': SinusGenerator(linspace_params=[0, 4, 100], amplitude=2),
+        'sg1': FiniteSinusGenerator(linspace_params=[0, 4, 100], amplitude=1, len_=50),
+        'l0': LambdaTransformer(lambda_x='lambda x: x*2+1'),
+        'l1': LambdaTransformer(lambda_x='lambda x: x**2'),
+        'sum': TimeSeriesSum(),
+        'not_used': NotUsed(),
+    }
+    cfg = yaml.load("""
+        =Z:
+          l1:
+            l0:
+              sg1
+        =K:
+          sg0
+        =X:
+          sum:
+            - Z
+            - K
+        =Y:
+          l1:
+            Z
+        """, yaml.FullLoader)
+
+    graph = make_graph(tsgs, cfg)
+    assert 'Z' in graph and isinstance(graph['Z'], TimeSeriesGen)
+    assert 'X' in graph and isinstance(graph['X'], TimeSeriesGen)
+    x_gen = graph['X']
+    y_gen = graph['Y']
+    k_gen = graph['K']
+    z_gen = graph['Z']
+    x_gen.build()
+    x_gen.build()
+    tsgs['l0'].config['lambda'] = 'None'
+    tsgs['l1'].config['lambda'] = 'None'
+    assert k_gen.is_infinite()
+    assert not x_gen.is_infinite()
+    assert len(x_gen) == 50
+    assert z_gen.getitem(0) == Signal(data_arr=(np.sin(np.linspace(0, 4, 100))*2+1)**2)
+    assert x_gen.getitem(0) == Signal(data_arr=(np.sin(np.linspace(0, 4, 100))*2+1)**2+2*np.sin(np.linspace(0, 4, 100)))
+    assert z_gen.getitem(0) == Signal(data_arr=(np.sin(np.linspace(0, 4, 100))*2+1)**2)
+    assert y_gen.getitem(0) == Signal(data_arr=((np.sin(np.linspace(0, 4, 100))*2+1)**2)**2)
+    assert tsgs['sg1'].history == [0]
+
+    assert y_gen.getitem(1) == Signal(data_arr=((np.sin(1+np.linspace(0, 4, 100))*2+1)**2)**2)
+    assert x_gen.getitem(1) == Signal(data_arr=(np.sin(1+np.linspace(0, 4, 100))*2+1)**2+2*np.sin(1+np.linspace(0, 4, 100)))
+    assert y_gen.getitem(0) == Signal(data_arr=((np.sin(np.linspace(0, 4, 100))*2+1)**2)**2)
+    assert tsgs['sg1'].history == [0, 1, 0]
+    assert 'epoch' not in tsgs['sg0'].config
+    x_gen.set_epoch(1)
+    assert tsgs['sg0'].config['epoch'] == 1
