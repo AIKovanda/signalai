d5ed57a6b52abc6aeb91f34d7c98a06172d88d41
Martin Kovanda <kovanda.physics@gmail.com>
2022-12-05 13:58:00 -0700

    random_cyclic_plastic_loading

---
+++ src/signalai/datasets/random_cyclic_plastic_loading.py
@@ -0,0 +1,133 @@
+import numpy as np
+from numba import jit
+
+from signalai.time_series import TimeSeries
+from signalai.time_series_gen import TimeSeriesGen
+
+SQR23 = np.sqrt(2. / 3.)
+SQR32 = np.sqrt(1.5)
+
+
+class RandomCyclicPlasticLoadingParamsV1:
+    def __init__(self, params: dict = None, scaled_params: np.ndarray = None, depspc_r=None):
+        assert (params is None) ^ (scaled_params is None)  # xor
+        self.params = params if params is not None else self.unscale_params(scaled_params)
+        self.depspc_r = depspc_r
+
+    @property
+    def scaled_params(self):
+        return np.array([
+            self._normalize_uniform(self.params['k0'], (150, 250)),
+            self._normalize_uniform(self.params['kap'][0], (100, 10000)),
+            self._normalize_uniform(1 / self.params['kap'][1], (30, 150)),
+            (np.log(self.params['c'][0]) - 8.07521718) / 0.64220986,
+            (np.log(self.params['c'][1]) - 6.66012441) / 0.70230589,
+            (np.log(self.params['c'][2]) - 5.75443793) / 0.82282555,
+            (np.log(self.params['c'][3]) - 4.83443022) / 0.71284407,
+            *[(i - 75) / 42.8 for i in self.params['a']],
+        ])
+
+    def unscale_params(self, scaled_params: np.ndarray):
+        len_a = int((len(scaled_params) - 3) / 2)
+        assert len_a == 4
+        return {
+            'k0': self._denormalize_uniform(scaled_params[0], (150, 250)),
+            'kap': [self._denormalize_uniform(scaled_params[1], (100, 10000)),
+                    1 / self._denormalize_uniform(scaled_params[2], (30, 150))],
+            'c': np.exp(np.array([
+                np.log(scaled_params[3]) * 0.64220986 + 8.07521718,
+                np.log(scaled_params[4]) * 0.70230589 + 6.66012441,
+                np.log(scaled_params[5]) * 0.82282555 + 5.75443793,
+                np.log(scaled_params[6]) * 0.71284407 + 4.83443022,
+            ])),
+            'a': scaled_params[7:] * 42.8 + 75,
+        }
+
+    @staticmethod
+    def _uniform_params(interval: tuple[float, float]) -> tuple[float, float]:
+        a, b = interval
+        assert b > a
+        return (a + b) / 2, (b - a) / np.sqrt(12)
+
+    def _normalize_uniform(self, num, interval: tuple[float, float]) -> float:
+        mu, sigma = self._uniform_params(interval)
+        return (num - mu) / sigma
+
+    def _denormalize_uniform(self, num, interval: tuple[float, float]) -> float:
+        mu, sigma = self._uniform_params(interval)
+        return num * sigma + mu
+
+    @classmethod
+    def generate(cls, depspc_r=None, depspc_r_number: int = None):
+        a = np.random.uniform(0, 1, size=4)
+        a /= np.sum(a)
+        return RandomCyclicPlasticLoadingParamsV1(params={
+            # 'E': np.random.uniform(160, 210),  # GPa
+            'k0': np.random.uniform(150, 250),
+            'kap': [np.random.uniform(100, 10000), 1 / np.random.uniform(30, 150)],  # MPa
+            'c': np.sort([np.exp(np.random.uniform(np.log(1000), np.log(10000))),
+                          *np.exp(np.random.uniform(np.log(50), np.log(2000), size=3))])[::-1],
+            'a': np.random.uniform(250, 350) * a,
+        },
+            depspc_r=np.random.uniform(0.0005, 0.005, depspc_r_number) if depspc_r is None else np.array(depspc_r),
+        )
+
+
+@jit(nopython=True)
+def _random_cyclic_plastic_loading(kap: np.ndarray, k0: float, a: np.ndarray, c: np.ndarray, depspc_r: np.ndarray, steps: int):
+    epspc = np.zeros(len(depspc_r) * steps + 1, dtype=np.float32)
+    kiso = np.zeros(len(depspc_r) * steps + 1, dtype=np.float32)
+    alp = np.zeros((len(a), len(depspc_r) * steps + 1), dtype=np.float32)
+
+    for i, next_depspc_r in enumerate(depspc_r):  # next_depspc_r does not start with 0!
+        D = (-1) ** i
+        for j in range(1, steps + 1):
+            epspc[i * steps + j] = epspc[i * steps] + j * next_depspc_r / steps
+            depspc = j * next_depspc_r / steps
+
+            kiso[i * steps + j] = D / kap[1] * (1 - (1 - kap[1] * k0) * np.exp(
+                -SQR32 * kap[0] * kap[1] * epspc[i * steps + j]))
+            alp[:, i * steps + j] = D * a - (D * a - alp[:, i * steps]) * np.exp(-c * depspc)
+
+    sig = SQR23 * np.sum(alp, axis=0) + kiso
+    return epspc, sig
+
+
+def random_cyclic_plastic_loading(rcpl_params: RandomCyclicPlasticLoadingParamsV1, steps: int) -> TimeSeries:
+    a = np.array(rcpl_params.params['a'], dtype=np.float32)
+    c = np.array(rcpl_params.params['c'], dtype=np.float32)
+    assert len(a) == len(c), 'a and c must be the same size'
+    assert rcpl_params.depspc_r[0] != 0, 'Leading zero is forbidden'
+    kap = np.array(rcpl_params.params['kap'], dtype=np.float32)
+    k0 = float(rcpl_params.params['k0'])
+    epspc, sig = _random_cyclic_plastic_loading(
+        kap=kap,
+        k0=k0,
+        a=a,
+        c=c,
+        depspc_r=np.array(rcpl_params.depspc_r, dtype=np.float32),
+        steps=int(steps),
+    )
+    return TimeSeries(
+        data_arr=sig,
+        meta={'rcpl_params': rcpl_params, 'epspc': epspc},
+    )
+
+
+class RandomCyclicPlasticLoadingGen(TimeSeriesGen):
+    def __len__(self):
+        raise ValueError
+
+    def _build(self):
+        assert 'depspc_r' in self.config or 'depspc_r_number' in self.config
+        assert 'steps' in self.config
+
+    def _getitem(self, _: int) -> TimeSeries:
+        model_params = RandomCyclicPlasticLoadingParamsV1.generate(
+            depspc_r=self.config.get("depspc_r"),
+            depspc_r_number=self.config.get("depspc_r_number"),
+        )
+        return random_cyclic_plastic_loading(model_params, steps=self.config["steps"])
+
+    def is_infinite(self) -> bool:
+        return True
---
+++ tests/datasets/test_random_cyclic_plastic_loading.py
@@ -0,0 +1,57 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from datasets.random_cyclic_plastic_loading import RandomCyclicPlasticLoadingGen, RandomCyclicPlasticLoadingParamsV1
+
+
+def test_norm():
+    for i in range(1, 12):
+        rcpl_params = RandomCyclicPlasticLoadingParamsV1.generate(12)
+        assert rcpl_params._denormalize_uniform(rcpl_params._normalize_uniform(i, [-1, 13]), [-1, 13]) == i
+        assert np.abs(np.exp(rcpl_params._denormalize_uniform(rcpl_params._normalize_uniform(np.log(i), [-1, 13]), [-1, 13])) - i) < 1e-8
+
+
+def test_params():
+    for i in range(10):
+        rcpl_params = RandomCyclicPlasticLoadingParamsV1.generate(12)
+        scaled = rcpl_params.scaled_params
+        print(scaled)
+        assert np.sum(np.abs(scaled - RandomCyclicPlasticLoadingParamsV1(scaled_params=scaled).scaled_params)) < 1e-15, scaled - RandomCyclicPlasticLoadingParamsV1(scaled_params=scaled).scaled_params
+
+
+def test_generation():
+    rtsg = RandomCyclicPlasticLoadingGen(depspc_r_number=12, steps=12)
+    sigs = []
+    params = []
+    for i in range(100000):
+        item = rtsg.getitem(i)
+        sigs.append(item.data_arr)
+        params.append(item.meta['rcpl_params'].scaled_params)
+    sigs = np.stack(sigs)
+    params = np.stack(params)
+    print('mu_abs', np.mean(np.abs(sigs)))
+    print('mu', np.mean(sigs))
+    print('std', np.std(sigs))
+
+    print('mu_pramas', np.mean(params, axis=0))
+    print('std_params', np.std(params, axis=0))
+
+    plt.figure()
+    for i in range(6):
+        print('-' * 12, i)
+        item = rtsg.getitem(i)
+        plt.plot(item.meta['epspc'], item.data_arr[0], linewidth=0.2)
+
+    plt.xlabel('$\\epsilon_p^c$')
+    plt.ylabel('$\\sigma$')
+    plt.savefig('out.svg')
+
+    plt.figure()
+    for i in range(6):
+        print('-' * 12, i)
+        item = rtsg.getitem(i)
+        plt.plot(list(range(len(item.meta['epspc']))), item.data_arr[0], linewidth=0.2)
+
+    plt.plot(list(range(len(item.meta['epspc']))), np.zeros_like(item.meta['epspc']), 'b.', markersize=1)
+    plt.ylabel('$\\sigma$')
+    plt.savefig('out_scaled.svg')
